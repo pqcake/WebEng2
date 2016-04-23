a6aba02234d7a214d8ebaf7444e29ab3691572da
Frank Maximilian <maximilian.frank@student.tuwien.ac.at>
2016-04-23 14:50:18 +0200

    Added recent viewed stuff using localstorage

--- src/main/webapp/scripts/framework.js
+++ src/main/webapp/scripts/framework.js
@@ -111,40 +111,64 @@ function formatCurrency(x) {
     return x.toFixed(2).replace(".", $("body").data('decimal-separator')).replace(/\B(?=(\d{3})+(?!\d))/g, $("body").data('grouping-separator')) + "&nbsp;€";
 }
 
-// Depending on the setup of your server, servlet, and socket, you may have to
-// change the URL.
-var socket = new WebSocket("ws://localhost:8080/socket");
-socket.onmessage = function (event) {
-    //testing stuff
-    console.log("event data: " + event.data);
-    if (event.data) {
-        var msg = JSON.parse(event.data);
-        switch(msg.type)
-        {
-            case "AUCTION_EXPIRED":
-                console.log("product id: "+ msg.productID);
-                console.log("current balance: " + msg.currentBalance);
-                console.log("runningBids: " + msg.runningBids);
-                console.log("wonAuctions: " + msg.wonAuctions);
-                console.log("lostAuctions: " + msg.lostAuctions);
-                //set attribute
-                $("[data-product-id="+ msg.productID + "] > a").toggleClass("expired");
-                $(".balance").text(msg.currentBalance);
-                $(".running-auctions-count").text(msg.runningBids);
-                $(".won-auctions-count").text(msg.wonAuctions);
-                $(".lost-auctions-count").text(msg.lostAuctions);
-                break;
-            case "NEW_BID":
-                
-                break;
-            case "OUTBIDDEN":
-                break;
+$(document).ready(function () {
 
+    //local storage
+    if (supportsLocalStorage()) {
+        // display
+        var journey = JSON.parse(localStorage.getItem('journey')) || {};
+        if (!jQuery.isEmptyObject(journey)) {
+            $(".recently-viewed-headline,.recently-viewed-list").css("display", "block");
+            jQuery.each(journey, function (link, name) {
+                var linkStr = '<li class="recently-viewed-link"><a href="' + link + '">' + name + '</a></li>';
+                $(".recently-viewed-list").append(linkStr);
+            });
+        }
+        // save
+        if ($("#productheadline").length) {
+            var headline = $("#productheadline");
+            var name = headline.text();
+            var link = location.pathname + location.search;
+            var pair = {};
+            journey[link] = name;
+            // journey.push(pair);
+            localStorage.setItem('journey', JSON.stringify(journey));
         }
     }
-    else
-    {
 
-    }
+// Depending on the setup of your server, servlet, and socket, you may have to
+// change the URL.
+    var socket = new WebSocket("ws://localhost:8080/socket");
+    socket.onmessage = function (event) {
+        //testing stuff
+        console.log("event data: " + event.data);
+        if (event.data) {
+            var msg = JSON.parse(event.data);
+            switch (msg.type) {
+                case "AUCTION_EXPIRED":
+                    console.log("product id: " + msg.productID);
+                    console.log("current balance: " + msg.currentBalance);
+                    console.log("runningBids: " + msg.runningBids);
+                    console.log("wonAuctions: " + msg.wonAuctions);
+                    console.log("lostAuctions: " + msg.lostAuctions);
+                    //set attribute
+                    $("[data-product-id=" + msg.productID + "] > a").toggleClass("expired");
+                    $(".balance").text(msg.currentBalance);
+                    $(".running-auctions-count").text(msg.runningBids);
+                    $(".won-auctions-count").text(msg.wonAuctions);
+                    $(".lost-auctions-count").text(msg.lostAuctions);
+                    break;
+                case "NEW_BID":
 
-};
+                    break;
+                case "OUTBIDDEN":
+                    break;
+
+            }
+        }
+        else {
+
+        }
+
+    };
+})
